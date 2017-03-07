499730238983e05d592797c503e70b5cb0b9e25f
Peter Schrammel <peter.schrammel@preisanalytics.de>
2017-03-07 08:27:14 +0000

    some specs

--- spec/ragent_spec.rb
+++ spec/ragent_spec.rb
@@ -2,7 +2,28 @@
 require 'spec_helper'
 
 describe Ragent do
+  let(:ragent) { Ragent.ragent }
+
   it 'has a version number' do
     expect(Ragent::VERSION).not_to be nil
+  end
+  
+  it "sets up nonblocking" do
+    Ragent.start(workdir: APP_DIR, log_level: 'debug', blocking: false)
+    expect(ragent).not_to be_nil
+  end
+  
+  describe "with setup ragent" do
+    before do
+      Ragent.start(workdir: APP_DIR, log_level: 'debug', blocking: false)
+    end
+    
+    it "has a workdir" do
+      expect(ragent.workdir.to_s).to eq(APP_DIR)
+    end
+    
+    it "has a templates_path" do
+      expect(ragent.templates_path.to_s).to eq(File.expand_path('../../../templates',APP_DIR))
+    end    
   end
 end
